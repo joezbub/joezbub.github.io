ce1dab057db7a4a2fe4d1b1e88d0122a9017d7c3
Joseph Zhang <josephzhang999@gmail.com>
2020-03-17 11:43:47 -0700

    personal site

--- backtohome.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $files = scandir('uploads', SCANDIR_SORT_DESCENDING);
-    $newest_file = "uploads/" . $files[0];
-    shell_exec("rm $newest_file");
-    header("Location: index.html");
-?>
